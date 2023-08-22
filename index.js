05fa97aa4494e2244e8a75f35663e94117393f45
Miguel-Val <chahpo@gmail.com>
2023-08-21 22:12:12 -0400

    style done, working on logic now

---
+++ index.js
@@ -0,0 +1,5 @@
+const grid = document.querySelector('.grid');
+const setPixels = document.querySelector('.16px');
+const thrityTwo = document.querySelector('.32px');
+const fortyEight = document.querySelector('.48px');
+const sixtyTwo = document.querySelector('.62px')
