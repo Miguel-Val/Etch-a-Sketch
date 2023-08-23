af2569340e6f654cbf42077c40f49664172c35e0
Miguel-Val <chahpo@gmail.com>
2023-08-22 21:09:39 -0400

    riged up the px buttons to match grid size

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 const grid = document.querySelector('.grid');
-const setPixels = document.querySelector('.16px');
-const thrityTwo = document.querySelector('.32px');
-const fortyEight = document.querySelector('.48px');
-const sixtyTwo = document.querySelector('.62px')
+const row = document.querySelector('.row');
+const defaultPx = document.querySelector('.sixTeen');
+const thrityTwo = document.querySelector('.thrityTwo');
+const fortyEight = document.querySelector('.fortyEight');
+const sixtyTwo = document.querySelector('.sixtyFour');
+
+
+defaultPx.addEventListener('click', () => {
+    
+    num(16)
+    
+})
+
+thrityTwo.addEventListener('click', () => {
+    
+    
+    num(32)
+})
+
+fortyEight.addEventListener('click', () => {
+    
+    num(48)
+})
+
+sixtyTwo.addEventListener('click', () => {
+    
+    num(62);
+})
+num(16);
+
+
+
+function num (gridSize) {
+    grid.innerHTML = '';
+    for (let i = 0; i < gridSize; i++) {
+        const newRow = document.createElement('div');
+        newRow.classList.add('row');
+        grid.append(newRow);
+
+        for (let j = 0; j < gridSize; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('gridDiv');
+            newRow.append(newDiv);
+        }
+    }
+    const gridWidth = 700;
+    const gridHeight = 700;
+
+    // Calculate box dimensions
+    const boxWidth = gridWidth / gridSize;
+    const boxHeight = gridHeight / gridSize;
+
+    const gridDivs = document.querySelectorAll('.gridDiv');
+    gridDivs.forEach((div) => {
+        div.style.width = `${boxWidth}px`;
+        div.style.height = `${boxHeight}px`;
+    });
+}
+ // Number of rows and columns
+
+
+
+// Create boxes for rows
+
+
+
+
