619539281f11c16b275f7e90aa79b26845edcbda
Miguel-Val <chahpo@gmail.com>
2023-08-27 04:29:36 -0400

    fixed colors button and rainbow button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Selecting DOM elements
 const grid = document.querySelector('.grid');
 const row = document.querySelector('.row');
 const defaultPx = document.querySelector('.sixTeen');
@@ -9,39 +10,63 @@ const gridDivs = document.querySelectorAll('.gridDiv');
 const changeMe = document.querySelectorAll('.changeMe');
 const eraserBtn = document.querySelector('.eraser');
 const colorBtn = document.querySelector('.color');
-const blue = document.querySelector('.blue')
-const green = document.querySelector('.green')
-const orange = document.querySelector('.orange')
-const yellow = document.querySelector('.yellow')
-const purple = document.querySelector('.purple')
-const red = document.querySelector('.red')
+const rainbowBtn = document.querySelector('.rainbow');
+const blue = document.querySelector('.blue');
+const green = document.querySelector('.green');
+const orange = document.querySelector('.orange');
+const yellow = document.querySelector('.yellow');
+const purple = document.querySelector('.purple');
+const red = document.querySelector('.red');
+const rainbowColors = ['#bae1ff', '#ffb3ba', '#FAC898', '#ffffba', '#C3B1E1', '#baffc9'];
+let rainbowColorIndex = 0;
+let currentColor;
 
-let isColoring = true; // Declare globally
+// Global state variables
+let isColoring = true;
 let isErasing = false;
 let isMouseDown = false;
 
+// Default grid setup
 defaultPx.addEventListener('click', () => {
-    gridFix(16)
-    divStyle(16)
+    gridFix(16);
+    divStyle(16);
 });
 thrityTwo.addEventListener('click', () => {
-    gridFix(32)
-    divStyle(32)
+    gridFix(32);
+    divStyle(32);
 });
 fortyEight.addEventListener('click', () => {
-    gridFix(48)
-    divStyle(48)
+    gridFix(48);
+    divStyle(48);
 });
 sixtyTwo.addEventListener('click', () => {
-    gridFix(62)
-    divStyle(62)
+    gridFix(62);
+    divStyle(62);
 });
 
 gridFix(16);
 divStyle(16);
 
+// Function to create the grid
+function gridFix(gridSize) {
+    grid.innerHTML = '';
+    for (let i = 0; i < gridSize; i++) {
+        const newRow = document.createElement('div');
+        newRow.classList.add('row');
+        grid.append(newRow);
+        newRow.setAttribute('draggable', 'false');
 
-function gridFix (gridSize) {
+        for (let j = 0; j < gridSize; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('gridDiv');
+            newRow.append(newDiv);
+            newDiv.setAttribute('draggable', 'false');
+        }
+    }
+}
+
+// Function to style grid cells
+function divStyle(gridSize) {
     grid.innerHTML = '';
     for (let i = 0; i < gridSize; i++) {
         const newRow = document.createElement('div');
@@ -60,7 +85,7 @@ function gridFix (gridSize) {
 }
 
 
- function divStyle (gridSize) {
+function divStyle (gridSize) {
     let isColoring;
     let isErasing;
     let isMouseDown;
@@ -81,63 +106,101 @@ function gridFix (gridSize) {
     colorBtn.addEventListener('click', () => {
         isErasing = false;
         isColoring = true;
-    })
+    });
+    let isRainbowMode = false;
+    rainbowBtn.addEventListener('click', () => {
+        isErasing = false;
+        isColoring = false;
+        isRainbowMode = true;
+    });
+
 
     blue.addEventListener('click', () => {
-        sum('blue')
+        isColoring = true;
+        isErasing = false;
+        currentColor = '#b2ebf2'
+        
+    })
+    green.addEventListener('click', () => {
+        isColoring = true;
+        isErasing = false;
+        currentColor = '#baffc9'
+        
+    })
+
+    yellow.addEventListener('click', () => {
+        isColoring = true;
+        isErasing = false;
+        currentColor = '#ffffba'
+        
+    })
+
+    orange.addEventListener('click', () => {
+        isColoring = true;
+        isErasing = false;
+        currentColor = '#FAC898'
+        
+    })
+
+    purple.addEventListener('click', () => {
+        isColoring = true;
+        isErasing = false;
+        currentColor = '#C3B1E1'
+        
+    })
+
+    red.addEventListener('click', () => {
+        isColoring = true;
+        isErasing = false;
+        currentColor = '#ffb3ba'
         
     })
 
 
+
+    // Event listeners for grid cells
     gridDivs.forEach((div) => {
-        div.addEventListener('mousedown', function sum (changeMes)  {
+        div.addEventListener('mousedown', () => {
             isMouseDown = true;
             if (isErasing) {
-                div.classList.remove('changeMe');
-                div.classList.add('gridDiv');
-            }else if (isColoring){
-                div.classList.remove('gridDiv');
-                div.classList.add(changeMes);    
-            }else{
-                div.classList.remove('gridDiv');
-                div.classList.add('changeMe');
+                div.style.backgroundColor = 'white';
+            } else if (isColoring) {
+                div.style.backgroundColor = currentColor;
+            } else if (isRainbowMode) {
+                startRainbowMode(div);
+            } else {
+                div.style.backgroundColor = 'rgba(0, 0, 0, 0.904)';
             }
-            
         });
     
         div.addEventListener('mouseup', () => {
             isMouseDown = false;
+            isRainbowMode = false; // Reset rainbow mode
         });
     
         div.addEventListener('mousemove', () => {
             if (isMouseDown && isColoring) {
-                div.classList.remove('gridDiv');
-                div.classList.add('changeMe');
-            }else if(isMouseDown && isErasing){
-                div.classList.remove('changeMe');
-                div.classList.add('gridDiv');
-            }else if(isMouseDown){
-                div.classList.remove('gridDiv');
-                div.classList.add('changeMe');
+                div.style.backgroundColor = currentColor;
+            } else if (isMouseDown && isErasing) {
+                div.style.backgroundColor = 'white';
+            } else if (isMouseDown && isRainbowMode) {
+                startRainbowMode(div);
+            } else if (isMouseDown) {
+                div.style.backgroundColor = 'rgba(0, 0, 0, 0.904)';
             }
         });
     });
     
+    // Function to start rainbow mode
+    function startRainbowMode(div) {
+        div.style.backgroundColor = rainbowColors[rainbowColorIndex];
+        rainbowColorIndex = (rainbowColorIndex + 1) % rainbowColors.length;
+    }
+    
+    // Event listener for the "Clear" button
     clearBtn.addEventListener('click', () => {
-        gridDivs.forEach((div)=>{
-            div.classList.remove('changeMe');
-            div.classList.add('gridDiv');
-        })
-    })
- }
-
-
-// ... your other code ...
-
-
-
-
-
-
-
-
+        gridDivs.forEach((div) => {
+            div.style.backgroundColor = 'white';
+        });
+    });
+}
